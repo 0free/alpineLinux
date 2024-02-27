914d00acd9bda2032999692786802f35dbc61db6
Saif AlSubhi <saifalsubhi@live.com>
2024-02-27 13:21:30 +0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -881,7 +881,7 @@ change_root() {
     if ! grep -q 'step=' $f; then
         printf '\n%s' 'step=0' >> $f
         printf '%s\n' "❯ copying $f"
-        cp $f /mnt
+        cp $f /mnt/root/
     fi
 
     printf '%s\n' "❯ copying install script"
